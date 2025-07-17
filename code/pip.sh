33d8c1328f5f12b15073a9a915e53f69d39ef40d
55d <55137390+55Dupup@users.noreply.github.com>
2025-07-17 22:31:53 +0800

    Update pip.sh

--- code/pip.sh
+++ code/pip.sh
@@ -4,7 +4,6 @@ sudo apt install libgl1-mesa-glx
 pip3 install basicsr --index-url http://pypi.douban.com/simple --trusted-host pypi.douban.com
 pip3 install -r requirements.txt --index-url http://pypi.douban.com/simple --trusted-host pypi.douban.com
 pip3 install pandas --index-url http://pypi.douban.com/simple --trusted-host pypi.douban.com
-sudo python3 setup.py develop
 #sudo pip uninstall bytedmetrics
 pip3 install einops --index-url http://pypi.douban.com/simple --trusted-host pypi.douban.com
 pip3 install lpips --index-url http://pypi.douban.com/simple --trusted-host pypi.douban.com
